fd1d6cbe1b714eed92f72156c9c41a814b302129
Achal Urankar <achalurankar@gmail.com>
2022-12-31 20:15:27 +0530

    valid words filter added

--- src/Processor.js
+++ src/Processor.js
@@ -1,10 +1,12 @@
+import { dictionary } from "./data/dictionary"
+
 export default class Processor {
 
     DELIMITER = "_"
     output = ""
     
     getCombinations(word, letters) {
-        this.process(word, letters, this.getRequiredLength(word))
+        this.process(word.toLowerCase(), letters.toLowerCase(), this.getRequiredLength(word))
         return this.output.substring(0, this.output.length - 1).split(" ")
     }
 
@@ -20,20 +22,12 @@ export default class Processor {
     process(word, letters, requiredLength, currWord = "") {
         if(currWord.length >= requiredLength) {
             let populatedWord = this.populateWord(currWord, word);
-            // this.isWordValid(populatedWord)
-            this.output += populatedWord + " "
+            if(dictionary.includes(populatedWord))
+                this.output += populatedWord + " "
             return
         }
         for(let letter of letters) {
             this.process(word, letters, requiredLength, currWord + letter)
-        }
-    }
-
-    async isWordValid(word) {
-        const res = await fetch('https://api.dictionaryapi.dev/api/v2/entries/en/' + word)
-        const resCode = res.status;
-        if(resCode !== 404){
-            console.log(`${word} is valid`);
         }
     }
 
